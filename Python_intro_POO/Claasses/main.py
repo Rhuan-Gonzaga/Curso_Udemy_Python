62bcfe265d0cec55cf79f49ffa464dc5c805de3c
unknown <rhuangonzaga22@gmail.com>
2022-06-27 19:59:07 -0300

    Classes

---
+++ Python_intro_POO/Claasses/main.py
@@ -0,0 +1,20 @@
+from pessoa import Pessoa
+
+#OBJ
+p1 = Pessoa("Luiz", 21)
+p2 = Pessoa("Lucas", 25)
+p3 = Pessoa("Daniel", 55)
+
+
+
+#p1.comer("maça")
+#p1.falar("POO")
+#p1.parar_comer()
+#p1.falar("Django")
+#p1.comer("pizza")
+#p1.parar_falar()
+#p1.falar("Python")
+
+p1.falar("POO")
+p2.falar("Musicas")
+p3.comer("Pizza")
